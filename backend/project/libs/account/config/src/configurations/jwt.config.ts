49168d90617f3e24ac3f8bbb1d598c779bdbede6
Алина Вдовиченко <alinavdovichenko.ru@gmail.com>
2024-11-09 13:53:54 +0500

    59. Добавит настройки для формирования refresh token.

--- backend/project/libs/account/config/src/configurations/jwt.config.ts
+++ backend/project/libs/account/config/src/configurations/jwt.config.ts
@@ -4,11 +4,15 @@ import { registerAs } from '@nestjs/config';
 export interface JWTConfig {
   accessTokenSecret: string;
   accessTokenExpiresIn: string;
+  refreshTokenSecret: string;
+  refreshTokenExpiresIn: string;
 }
 
 const validationSchema = Joi.object({
   accessTokenSecret: Joi.string().required(),
   accessTokenExpiresIn: Joi.string().required(),
+  refreshTokenSecret: Joi.string().required(),
+  refreshTokenExpiresIn: Joi.string().required(),
 });
 
 
@@ -21,8 +25,10 @@ function validateConfig(config: JWTConfig): void {
 
 function getConfig(): JWTConfig {
   const config: JWTConfig = {
-    accessTokenSecret: 'fkbyfDljdbxtyrj17',
-    accessTokenExpiresIn: '1d',
+    accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
+    accessTokenExpiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
+    refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
+    refreshTokenExpiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRES_IN,
   };
 
   validateConfig(config);
